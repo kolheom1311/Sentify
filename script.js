082a5d0ac0d626d333ca6c6db1f707b86e8e292e
kolheom1311 <110466889+kolheom1311@users.noreply.github.com>
2024-03-10 00:39:33 +0530

    Functionality increased
    Login/SignUp is functional using Google Firebase

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const imageInput = document.getElementById("imageInput");
 const imageContainer = document.getElementById("imageContainer");
+//firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyAtDR1uMypbur1SgItz6zVFeKP5ebRapY0",
+  authDomain: "sentify-web.firebaseapp.com",
+  databaseURL: "https://sentify-web-default-rtdb.firebaseio.com",
+  projectId: "sentify-web",
+  storageBucket: "sentify-web.appspot.com",
+  messagingSenderId: "178179484502",
+  appId: "1:178179484502:web:9e859074982c5a119604f7",
+  measurementId: "G-N29DQTKBF7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth();
+// const auth = getAuth();
+const database = firebase.database();
 
 function redirectToLoginPage() {
   window.location.href = "Login-signup/login-signup.html";
 }
+
+function logout() {
+    // Sign out the user
+    // firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      alert('Logout successful!');
+      // Redirect the user to the login page or any other desired page
+      window.location.href = "Login-signup/login-signup.html"; // Change 'login.html' to the desired page
+    // }).catch(function(error) {
+    //   // An error happened.
+    //   console.error('Logout error:', error);
+    //   alert('Logout failed. Please try again.');
+    // });
+  }
 
 imageInput.addEventListener("change", function() {
   imageContainer.innerHTML = "";
